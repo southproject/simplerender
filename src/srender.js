0ac206caa1ce0fd3daf81b938d1ff52531cbb02f
southproject <yukipedia_yui@yahoo.com>
2019-01-02 11:59:26 +0800

    调整路径完毕，可能有细微bug

--- src/srender.js
+++ src/srender.js
@@ -2,22 +2,22 @@
 * SRender, a high performance 2d drawing library.
 */
 
-import guid from './core/guid';
-import env from './core/env';
-import * as zrUtil from './core/util';
-import Handler from './Handler';
-import Storage from './Storage';
-import ObjectList from './ObjectList';
-import Painter from './Painter';
-import Animation from './animation/Animation';
-import HandlerProxy from './dom/HandlerProxy';
+import guid from './util/core/guid';
+import env from './util/core/env';
+import * as zrUtil from './util/core/util';
+import Handler from './Handler/Handler';
+import Storage from './ObjectBase/Storage';
+import ObjectList from './ObjectBase/ObjectList';
+import Painter from './Render/Painter';
+import Animation from './Render/animation/Animation';
+import HandlerProxy from './Handler/dom/HandlerProxy';
 
 //import * as zrUtil from './core/util';
-import * as matrix from './core/matrix';
-import * as vector from './core/vector';
-import * as colorTool from './tool/color';
-import * as pathTool from './tool/path';
-import {parseSVG} from './tool/parseSVG';
+import * as matrix from './util/core/matrix';
+import * as vector from './util/core/vector';
+import * as colorTool from './util/tool/color';
+import * as pathTool from './util/tool/path';
+import {parseSVG} from './util/tool/parseSVG';
 
 var useVML = !env.canvasSupported;
 
@@ -89,36 +89,36 @@ function delInstance(id) {
 
 
 
-export {default as Group} from './container/Group';
-export {default as Path} from './graphic/Path';
-export {default as Image} from './graphic/Image';
-export {default as CompoundPath} from './graphic/CompoundPath';
-export {default as Text} from './graphic/Text';
-export {default as IncrementalDisplayable} from './graphic/IncrementalDisplayable';
+export {default as Group} from './Render/container/Group';
+export {default as Path} from './Element/graphic/Path';
+export {default as Image} from './Element/graphic/Image';
+export {default as CompoundPath} from './Element/graphic/CompoundPath';
+export {default as Text} from './Element/graphic/Text';
+export {default as IncrementalDisplayable} from './Element/graphic/IncrementalDisplayable';
 
-export {default as Arc} from './graphic/shape/Arc';
-export {default as BezierCurve} from './graphic/shape/BezierCurve';
-export {default as Circle} from './graphic/shape/Circle';
-export {default as Droplet} from './graphic/shape/Droplet';
-export {default as Ellipse} from './graphic/shape/Ellipse';
-export {default as Heart} from './graphic/shape/Heart';
-export {default as Isogon} from './graphic/shape/Isogon';
-export {default as Line} from './graphic/shape/Line';
-export {default as Polygon} from './graphic/shape/Polygon';
-export {default as Polyline} from './graphic/shape/Polyline';
-export {default as Rect} from './graphic/shape/Rect';
-export {default as Ring} from './graphic/shape/Ring';
-export {default as Rose} from './graphic/shape/Rose';
-export {default as Sector} from './graphic/shape/Sector';
-export {default as Star} from './graphic/shape/Star';
-export {default as DbCircle} from './graphic/shape/DbCircle';
-export {default as House} from './graphic/shape/House';
-export {default as Trochoid} from './graphic/shape/Trochoid';
+export {default as Arc} from './Element/graphic/shape/Arc';
+export {default as BezierCurve} from './Element/graphic/shape/BezierCurve';
+export {default as Circle} from './Element/graphic/shape/Circle';
+export {default as Droplet} from './Element/graphic/shape/Droplet';
+export {default as Ellipse} from './Element/graphic/shape/Ellipse';
+export {default as Heart} from './Element/graphic/shape/Heart';
+export {default as Isogon} from './Element/graphic/shape/Isogon';
+export {default as Line} from './Element/graphic/shape/Line';
+export {default as Polygon} from './Element/graphic/shape/Polygon';
+export {default as Polyline} from './Element/graphic/shape/Polyline';
+export {default as Rect} from './Element/graphic/shape/Rect';
+export {default as Ring} from './Element/graphic/shape/Ring';
+export {default as Rose} from './Element/graphic/shape/Rose';
+export {default as Sector} from './Element/graphic/shape/Sector';
+export {default as Star} from './Element/graphic/shape/Star';
+export {default as DbCircle} from './Element/graphic/shape/DbCircle';
+export {default as House} from './Element/graphic/shape/House';
+export {default as Trochoid} from './Element/graphic/shape/Trochoid';
 
-export {default as LinearGradient} from './graphic/LinearGradient';
-export {default as RadialGradient} from './graphic/RadialGradient';
-export {default as Pattern} from './graphic/Pattern';
-export {default as BoundingRect} from './core/BoundingRect';
+export {default as LinearGradient} from './Element/graphic/LinearGradient';
+export {default as RadialGradient} from './Element/graphic/RadialGradient';
+export {default as Pattern} from './Element/graphic/Pattern';
+export {default as BoundingRect} from './util/core/BoundingRect';
 
 export {matrix};
 export {vector};
